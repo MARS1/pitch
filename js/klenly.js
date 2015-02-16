d2cfd6778505a3f3ad62df9e6452b3432498ef43
Marcelo Berta <marcelo.berta@gmail.com>
2015-02-15 20:45:05 -0500

    completed first slide and adjusted styles

---
+++ js/klenly.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    Reveal.initialize({
+
+    // The "normal" size of the presentation, aspect ratio will be preserved
+    // when the presentation is scaled to fit different resolutions. Can be
+    // specified using percentage units.
+    width: '100%',
+    height: '100%',
+
+    // Factor of the display size that should remain empty around the content
+    margin: 0,
+
+    // Bounds for smallest/largest possible scale to apply to content
+    minScale: 0.2,
+    maxScale: 1.5
+
+});
+});
